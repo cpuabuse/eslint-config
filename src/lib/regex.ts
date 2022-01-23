72491948589f94fc7a39178478665e3dcea6666e
Herman <35882542+winterhotlatte@users.noreply.github.com>
2022-01-23 15:13:00 +0900

    Fix: Regex first line

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -8,14 +8,29 @@
  */
 
 /**
+ * Backtick character.
+ */
+const backtick: string = "`";
+
+/**
+ * Numbers.
+ */
+const numbers: string = "\\d";
+
+/**
+ * First letter in English sentences, that are not used for Markdown.
+ */
+const englishFirstCharacter: string = `A-Z${backtick}"'{`;
+
+/**
  * Characters in the middle of English sentence, with characters needed for inline tags, that are not used for Markdown.
  */
-const englishMiddleCharacters: string = "A-Za-z \\d`{@#.:}\"',\\-;";
+const englishMiddleCharacters: string = `$A-Za-z ${numbers}${backtick}"'{@#.:},-;`;
 
 /**
  * Sentence following a tag immediately.
  */
-const firstLineSentence: string = "([A-Z`{\\d].*)([^.]|...)";
+const firstLineSentence: string = `[${englishFirstCharacter}${numbers}].*([^.])`;
 
 /**
  * Possible paragraphs in a comment.
@@ -24,24 +39,34 @@ const paragraphs: Record<string, string> = {
 	// Represents a paragraph that is a markdown codeblock.
 	code: "(```.*\\n((?!```).+\\n(\\n?|(?=```)))+```)(\\n{2}(?=.)|(?=$))",
 	// Represents a paragraph in English.
-	english: `([A-Z\`{][${englishMiddleCharacters}]*[.]\\n)*[A-Z\`{][${englishMiddleCharacters}]*[.](\\n{2}(?=.)|(?=$))`,
+	english: `([${englishFirstCharacter}][${englishMiddleCharacters}]*[.]\\n)*[${englishFirstCharacter}][${englishMiddleCharacters}]*[.](\\n{2}(?=.)|(?=$))`,
 	// Represents a paragraph with special characters as the first symbol.
-	specialBeginning: "(.+\\n)*[^A-Za-z`{\\n].*((\\n.+)*\\n{2}(?=.)|(\\n.+)*(?=$))",
+	specialBeginning: `(.+\\n)*[^${englishFirstCharacter}a-z\\n].*((\\n.+)*\\n{2}(?=.)|(\\n.+)*(?=$))`,
 	// Represents a paragraph with special characters as the middle or last symbol.
 	specialRest: `(.+\\n)*.*[^${englishMiddleCharacters}\\n].*((\\n.+)*\\n{2}(?=.)|(\\n.+)*(?=$))`
 };
 
 /**
  * Regular expression for long comments.
+ *
+ * Identifies comments consisting of paragraphs, separated by two newlines. Each paragraph is identified by one of the following:
+ * - Paragraph in English, that does not contain Markdown characters, or start with characters for Markdown
+ * - Paragraph, containing Markdown characters
+ * - Paragraph, starting with a character, that could identify Markdown
+ * - Code block
  */
 export const blockTagRegex: string = `^(${Object.values(paragraphs).join("|")})+$`;
 
 /**
  * Regular expression for block tag with first line optional.
+ *
+ * Identifies {@link blockTagRegex | block sentences}, optionally preceded by {@link firstLineTagRegex | first line sentence}, for example title in `example` tag.
  */
 export const blockFirstLineTagRegex: string = `^(${firstLineSentence}\n)?(${Object.values(paragraphs).join("|")})+$`;
 
 /**
  * Regular expression for short comments.
+ *
+ * Identifies first line only tags, that begin with sensible character, and do not end with a period.
  */
 export const firstLineTagRegex: string = `^${firstLineSentence}$`;
